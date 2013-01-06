27af8bd55e86e6489e3e3922d2e079cc231fd349
Lauri Kasanen <curaga@operamail.com>
2013-01-06 13:49:26 +0200

    Initial things

--- frametime.c
+++ frametime.c
@@ -1,1 +1,16 @@
+#define _GNU_SOURCE
+
 #include <stdio.h>
+#include <dlfcn.h>
+#include <GL/glx.h>
+
+void (*realswap)(Display *dpy, GLXDrawable drawable) = NULL;
+
+void glXSwapBuffers(Display *dpy, GLXDrawable drawable) {
+	puts("Swappan");
+}
+
+static void init() __attribute__((constructor));
+static void init() {
+	puts("Buildan");
+}
